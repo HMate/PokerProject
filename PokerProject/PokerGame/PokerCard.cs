3b3dc2de3d523f51cc3b59f80ef3e8e963c4b5ce
Máté Hidvégi <mhidvegi@gmail.com>
2014-02-14 17:22:14 +0100

    first commit

---
+++ PokerProject/PokerGame/PokerCard.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerProject.PokerGame
+{
+    public class PokerCard
+    {
+        private CardSuite cardSuite;
+        private CardRank cardRank;
+
+
+        public PokerCard(CardRank rank, CardSuite suite)
+        {
+            cardRank = rank;
+            cardSuite = suite;
+        }
+
+        public PokerCard() : this(CardRank.Ace, CardSuite.Spades)
+        {
+            
+        }
+
+        public CardSuite CardSuite
+        {
+            get
+            {
+                return cardSuite;
+            }
+         }
+
+        public CardRank CardRank
+        {
+            get
+            {
+                return cardRank;
+            }
+
+        }
+
+
+    }
+}
---
+++ PokerUnitTest/CardTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PokerProject.PokerGame;
+
+namespace PokerUnitTest
+{
+    [TestClass]
+    public class PokerCardTest
+    {
+        [TestMethod]
+        public void CreatePokerCard()
+        {
+            PokerCard card = new PokerCard(CardRank.Ace, CardSuite.Spades);
+            PokerCard card2 = new PokerCard();
+            Assert.IsNotNull(card);
+            Assert.IsNotNull(card2);
+        }
+
+        [TestMethod]
+        public void PokerCardGetterTest()
+        {
+            PokerCard card = new PokerCard(CardRank.King, CardSuite.Clubs);
+            CardSuite testedSuite = card.CardSuite;
+            CardRank testedRank = card.CardRank;
+
+            Assert.AreEqual(testedRank, CardRank.King);
+            Assert.AreEqual(testedSuite, CardSuite.Clubs);
+        }
+
+
+
+    }
+}
